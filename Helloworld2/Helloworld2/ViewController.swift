7dd52437837b63e52496688f25cea2ed9ea4a9dd
Mechanicalbird <mechanicalbird_just@yahoo.com>
2017-07-12 12:47:35 +0900

    Add files via upload

---
+++ Helloworld2/Helloworld2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Helloworld2
+//
+//  Created by USER on 2017/07/12.
+//  Copyright © 2017 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
